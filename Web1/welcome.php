ad77809f628ce8aeda9c89dd37d7311389d4d929
haupham1304 <74724674+haupham1304@users.noreply.github.com>
2021-11-17 15:02:26 +0700

    create 2 web

---
+++ Web1/welcome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<!-- Page header -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+    <header>
+        <h3 class="title">
+            Chào mừng bạn đến với web 1
+        </h3>
+    </header>
+</body>
+
+</html>
---
+++ Web2/config.php
@@ -0,0 +1,5 @@
+<?php
+define('HOST', '127.0.0.1:3306');
+define('DATABASE', 'web2');
+define('USERNAME', 'root');
+define('PASSWORD', '');
